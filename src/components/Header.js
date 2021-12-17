07a589caa58ce5e68aac959a327dcc33fd19ae92
Scott Hutley <scotthutley1@comcast.net>
2021-12-17 09:53:44 -0800

    WIP temp header styling; creating custom header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,12 @@
 import React from "react";
 
 function Header(){
+  const headerStyle = {
+    textAlign: "center",
+    color: "purple"
+  }
   return (
-    <h1>Dedicated Cabinet</h1>
+    <h1 style={headerStyle}>Dedicated Cabinet</h1>
   );
 }
 
