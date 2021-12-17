a037be5d1beb6871d76c33c078b7d186d35c822b
Scott Hutley <scotthutley1@comcast.net>
2021-12-17 13:43:49 -0800

    WIP building out GameList function, unsure on return

--- src/components/GameList.js
+++ src/components/GameList.js
@@ -1,12 +1,25 @@
 import React from "react";
+import Game from "./Game";
+import PropTypes from "prop-types";
 
 function GameList() {
-  if (GameList.length ===  0) {
-  return (
-    <div>
-    <h1>No games yet!</h1>
-    </div>
-  )}
+  if (GameList.length > 0) {
+    return (
+      GameList()
+    );
+    } else {
+    return (
+      <div>
+      <h1>No games yet!</h1>
+      </div>
+    );
+  }
+}
+
+
+
+GameList.propTypes = {
+
 }
 
 export default GameList;
