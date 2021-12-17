adb080ccc4b39ebe50fbbdc93607dd274923c107
Scott Hutley <scotthutley1@comcast.net>
2021-12-17 14:00:24 -0800

    Add mainGameList dummy data to GameList

--- src/components/GameList.js
+++ src/components/GameList.js
@@ -2,11 +2,26 @@ import React from "react";
 import Game from "./Game";
 import PropTypes from "prop-types";
 
+const mainGameList = [
+{
+  name:  "NBA Jam Tournament Edition",
+  manufacturer:  "Midway Games",
+  releaseDate:  "1993/1994",
+  monitor:  "Horizontal",
+  players: "4 (Single player, Two-player co-op, One-vs-One, Two-vs-Two)",
+  controls: "Joystick: 8-way Buttons: 3 (Shoot Pass Turbo)",
+  cabinet: "Upright/Standard"
+}
+];
+
 function GameList() {
   if (GameList.length > 0) {
     return (
-      GameList()
-    );
+      {mainGameList.map((game, index) =>
+        <Game names={game.name}
+        key={index}/>
+        )}
+      );
     } else {
     return (
       <div>
