2eb15d628ec8ef022782fb4f81c51a6d0f076935
Scott Hutley <scotthutley1@comcast.net>
2021-12-21 08:25:36 -0800

    WIP testing renders, hard-coded "testName" showing

--- src/components/GameDetail.js
+++ src/components/GameDetail.js
@@ -6,14 +6,14 @@ function GameDetail(props) {
 
   return (
     <React.Fragment>
-      <h1>{game.name}</h1>
+      {/* <h1>{game.name}</h1>
       <p>{game.manufacturer}</p>
       <p>{game.releaseDate}</p>
       <p>{game.monitor}</p>
       <p>{game.players}</p>
       <p>{game.controls}</p>
       <p>{game.cabinet}</p>
-      <hr />
+      <hr /> */}
     </React.Fragment>
   );
 }
