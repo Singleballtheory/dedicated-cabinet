008e63d4354f69e03f49e4821a08f4bea13f0cf2
Scott Hutley <scotthutley1@comcast.net>
2021-12-21 15:13:15 -0800

    WIP testing mouse-over tooltips, not working yet

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,13 +1,27 @@
 import React from "react";
 import PropTypes from "prop-types";
+import DedCab from "./img/DedicatedCabinet.png";
 
 function NavBar(props) {
+  const avatar = {
+    width: "100px",
+    height: "100px",
+    borderRadius: "50%",
+    backgroundSize: "cover",
+    backgroundPosition: "center",
+    content: attr(dataTooltip),
+
+  }
 
   return (
+    <React.Fragment>
+    <div style={avatar}>Anything here?<img src={DedCab}/>
+    </div>
     <div>
       <button className="buttons" onClick={() => props.onAboutClick()}>About</button>
     </div>
-  )
+    </React.Fragment>
+  );
 }
 
 NavBar.propTypes = {
