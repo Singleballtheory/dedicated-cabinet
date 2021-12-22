c154693bf53483eda7e41a741b6f08874cf92ce4
Scott Hutley <scotthutley1@comcast.net>
2021-12-22 12:26:50 -0800

    Add bg image to App.js; update Nav.css styles

--- src/components/App.js
+++ src/components/App.js
@@ -2,9 +2,15 @@ import React from "react";
 import GameControl from "./GameControl";
 import Header from "./Header";
 import NavBar from "./NavBar";
+import GamePhoto from "./img/games.JPG";
 // import './App.css';
 
 function App() {
+  const NavStyle = {
+    width: "1500px",
+    height: "800px",
+  };
+
   return (
     <React.Fragment>
     <div>
@@ -14,6 +20,7 @@ function App() {
       <NavBar />
     </div>
     <div>
+      <img style={NavStyle} src={GamePhoto}/>
       <GameControl />
     </div>
     </React.Fragment>
