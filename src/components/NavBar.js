7f1d249fd24702fde031bfeaf939117d21e27484
Scott Hutley <scotthutley1@comcast.net>
2021-12-22 10:36:01 -0800

    FIX tooltip in Nav.css; add image import to NavBar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,12 +1,13 @@
 import React from "react";
 import PropTypes from "prop-types";
 import "./../Nav.css";
+import GamePhoto from "./img/games.JPG";
 
 function NavBar(props) {
 
   return (
     <React.Fragment>
-    <div class="avatar" data-tooltip="Thinking Cat">Anything here?
+    <div class="avatar" data-tooltip="Thinking Cat" style={{backgroundImage: `url(${GamePhoto})`}}>
     </div>
     <div>
       <button className="buttons" onClick={() => props.onAboutClick()}>About</button>
