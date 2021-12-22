ded3d85de62df75529a33ab79a8e54e16da334b0
Scott Hutley <scotthutley1@comcast.net>
2021-12-21 16:52:27 -0800

    WIP still working through tooltip lesson

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,21 +1,12 @@
 import React from "react";
 import PropTypes from "prop-types";
-import DedCab from "./img/DedicatedCabinet.png";
+import "./../Nav.css";
 
 function NavBar(props) {
-  const avatar = {
-    width: "100px",
-    height: "100px",
-    borderRadius: "50%",
-    backgroundSize: "cover",
-    backgroundPosition: "center",
-    content: attr(dataTooltip),
-
-  }
 
   return (
     <React.Fragment>
-    <div style={avatar}>Anything here?<img src={DedCab}/>
+    <div class="avatar" data-tooltip="Thinking Cat">Anything here?
     </div>
     <div>
       <button className="buttons" onClick={() => props.onAboutClick()}>About</button>
